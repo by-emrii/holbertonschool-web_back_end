3c328102e18567bcfb37398d0e8cc25fbc968d16
Emily Chew <emilychewsh@gmail.com>
2025-11-21 15:31:45 +1100

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This is for python module '5-sum_list.py' """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns sum as a float."""
+    return sum(input_list)
