e446a7f0ffdf4b2e6d343f7a022c51891551906a
Emily Chew <emilychewsh@gmail.com>
2025-11-21 15:14:35 +1100

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is for python module '3-to_str.py' """
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    returns the string representation of the float
+    """
+    return str(n)
