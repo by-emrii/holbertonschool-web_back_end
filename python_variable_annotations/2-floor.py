422c7058e3df413a59dec16052351c6f228c3039
Emily Chew <emilychewsh@gmail.com>
2025-11-21 15:12:04 +1100

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is for python module '2-floor.py' """
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of the float
+    """
+    return math.floor(n)
