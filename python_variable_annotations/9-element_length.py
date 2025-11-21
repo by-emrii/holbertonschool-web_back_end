e522394005bbf63e851ac5ce3cd655ec870eb256
Emily Chew <emilychewsh@gmail.com>
2025-11-21 16:00:17 +1100

    task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This is for python module '9-element_length.py' """
-from typing import List, Iterable, Sized, Tuple
+from typing import List, Iterable, Sequence, Tuple
 
 
-def element_length(lst: Iterable[Sized]) -> List[Tuple[Sized, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return length"""
     return [(i, len(i)) for i in lst]
