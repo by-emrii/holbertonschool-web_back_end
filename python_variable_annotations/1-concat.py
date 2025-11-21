5730ae230657350f01f440d57ab5f7f047c89452
Emily Chew <emilychewsh@gmail.com>
2025-11-21 15:06:45 +1100

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This is for python module '1-concat.py' """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenates 2 strings together
+    """
+    return str1 + str2
