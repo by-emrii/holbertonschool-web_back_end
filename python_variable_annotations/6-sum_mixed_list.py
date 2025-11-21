4c63e3fb37fd5f657d741d076a202d0a34dddfd7
Emily Chew <emilychewsh@gmail.com>
2025-11-21 15:38:42 +1100

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This is for python module '6-sum_mixed_list.py' """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+    """returns sum as a float."""
+    return sum(mxd_list)
