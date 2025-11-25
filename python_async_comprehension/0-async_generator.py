e5f9577eeace88365bbfb749fbd2f82cf719dc8c
Emily Chew <emilychewsh@gmail.com>
2025-11-25 15:47:17 +1100

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """This is for python module '0-async_generator.py' """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     coroutine loop 10 times, each wait 1 second asynchronously,
     then yield a random number between 0 and 10
