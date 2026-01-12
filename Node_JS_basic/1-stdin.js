60335e73c46a3ea0a527cfddfe3fcff696aa2aff
Emily Chew <emilychewsh@gmail.com>
2026-01-12 17:07:45 +1100

    Task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,6 @@
-#!/usr/bin/node
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdout.on('data', (data) => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString().trim()}`);
 });
 
