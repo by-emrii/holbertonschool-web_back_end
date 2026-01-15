c593294d776b33d4ebc6c4730b71264caecb4237
Emily Chew <emilychewsh@gmail.com>
2026-01-15 17:48:47 +1100

    Task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  let data;
+
+  try {
+    // read files synchronously
+    data = await fs.readFileSync(path, 'utf-8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  // splits file into individual lines, filter out whitespaces etc
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+  // remove header
+  const students = lines.slice(1);
+  // console.log(students);
+
+  console.log(`Number of students: ${students.length}`);
+
+  // object to group students by field
+  const fields = {};
+  // looping through each line
+  students.forEach((line) => {
+    // split the CSV row by commas
+    const individual = line.split(',');
+    const firstName = individual[0];
+    const field = individual[3];
+
+    // if field does not exist yet, create an empty array
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+
+    // add students firstname to correct field array
+    fields[field].push(firstName);
+  });
+
+  // loop through each field in fields obj
+  for (const field in fields) {
+    if (fields[field]) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  }
+}
+
+module.exports = countStudents;
