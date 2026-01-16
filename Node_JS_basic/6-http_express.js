8beac42b1f82e0073a45ad51ca8ebf55c26ebeed
Emily Chew <emilychewsh@gmail.com>
2026-01-16 12:11:39 +1100

    task 6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245, (error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log('Express server running on port 1245');
+});
+
+module.exports = app;
